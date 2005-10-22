6d01624f99386abfe02096cc10e84195c53c0378
dtownsend <dtownsend@oxymoronical.com>
2005-10-22 16:14:14 +0000

    Added locales and properties

---
+++ defaults/preferences/tabsidebar.js
@@ -0,0 +1,3 @@
+pref("extensions.TabSidebar@blueprintit.co.uk.description", "chrome://tabsidebar/locale/tabsidebar.properties");
+pref("tabsidebar.refreshrate", 120000);
+pref("tabsidebar.drawscrolled", true);
