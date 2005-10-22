1c73d711a58b5170851497c13fd8821519c2265f
dtownsend <dtownsend@oxymoronical.com>
2005-10-22 16:50:51 +0000

    Added code to support configuration through preferences

--- defaults/preferences/tabsidebar.js
+++ defaults/preferences/tabsidebar.js
@@ -1,3 +1,4 @@
 pref("extensions.TabSidebar@blueprintit.co.uk.description", "chrome://tabsidebar/locale/tabsidebar.properties");
 pref("tabsidebar.refreshrate", 120000);
 pref("tabsidebar.drawscrolled", true);
+pref("tabsidebar.loaddelay", 500);
