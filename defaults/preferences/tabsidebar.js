e2bd3b18e6b24119bd558c7fa8a67112b86052d4
dtownsend <dtownsend@oxymoronical.com>
2005-10-23 16:53:59 +0000

    Seperated buttons to be customisable.

--- defaults/preferences/tabsidebar.js
+++ defaults/preferences/tabsidebar.js
@@ -8,7 +8,10 @@ pref("tabsidebar.loadrefreshrate", 1000);
 pref("tabsidebar.loadrefresh", false);
 pref("tabsidebar.showadvanced", false);
 
-pref("tabsidebar.content.buttons", true);
+pref("tabsidebar.content.security", true);
+pref("tabsidebar.content.close", true);
+pref("tabsidebar.content.reload", true);
+pref("tabsidebar.content.throbber", true);
 pref("tabsidebar.content.preview", true);
 pref("tabsidebar.content.title", true);
 pref("tabsidebar.content.uri", true);
