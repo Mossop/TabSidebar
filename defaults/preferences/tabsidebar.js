0577d5cdee4137c1ae004d899b2a1d1eb4edc346
dtownsend <dtownsend@oxymoronical.com>
2005-11-30 23:24:27 +0000

    Mammoth options culling.

--- defaults/preferences/tabsidebar.js
+++ defaults/preferences/tabsidebar.js
@@ -1,22 +1,15 @@
 pref("extensions.TabSidebar@blueprintit.co.uk.description", "chrome://tabsidebar/locale/tabsidebar.properties");
-pref("tabsidebar.refreshrate", 120);
+pref("tabsidebar.refreshrate", 30);
 pref("tabsidebar.autorefresh", true);
-//pref("tabsidebar.drawscrolled", true);
 pref("tabsidebar.hidetabs", false);
 
 pref("tabsidebar.showoptions", false);
 pref("tabsidebar.showhelp", false);
 
-pref("tabsidebar.showadvanced", false);
 pref("tabsidebar.loaddelay", 25);
 pref("tabsidebar.loadrefreshrate", 25);
 pref("tabsidebar.loadrefresh", true);
 
-pref("tabsidebar.content.favicon", true);
-pref("tabsidebar.content.close", true);
 pref("tabsidebar.content.reload", true);
 pref("tabsidebar.content.stop", true);
-pref("tabsidebar.content.throbber", true);
-pref("tabsidebar.content.preview", true);
-pref("tabsidebar.content.title", true);
 pref("tabsidebar.content.uri", false);
