09ed49bf1ce9f48f77ca700e16dba5ca6d8f9cf8
dtownsend <dtownsend@oxymoronical.com>
2005-12-26 23:05:15 +0000

    Need to add listeners to all subframes of a document. This resolves bug 87 and probably others.

--- defaults/preferences/tabsidebar.js
+++ defaults/preferences/tabsidebar.js
@@ -1,17 +1,13 @@
 pref("extensions.TabSidebar@blueprintit.co.uk.description", "chrome://tabsidebar/locale/tabsidebar.properties");
 pref("extensions.tabsidebar.hidetabs", false);
 
+pref("extensions.tabsidebar.loaddelay", 100);
 pref("extensions.tabsidebar.changedelay", 25);
 pref("extensions.tabsidebar.scrolldelay", 200);
+pref("extensions.tabsidebar.unselectedloaddelay", 1000);
 pref("extensions.tabsidebar.unselectedchangedelay", 1000);
 pref("extensions.tabsidebar.unselectedscrolldelay", 1000);
-pref("extensions.tabsidebar.refreshrate", 30);
-pref("extensions.tabsidebar.autorefresh", false);
 
-pref("extensions.tabsidebar.showoptions", false);
-pref("extensions.tabsidebar.showhelp", false);
-
-pref("extensions.tabsidebar.loaddelay", 25);
 pref("extensions.tabsidebar.loadrefreshrate", 25);
 pref("extensions.tabsidebar.loadrefresh", true);
 
@@ -20,3 +16,6 @@ pref("extensions.tabsidebar.content.stop", true);
 pref("extensions.tabsidebar.content.forward", true);
 pref("extensions.tabsidebar.content.back", true);
 pref("extensions.tabsidebar.content.uri", false);
+
+pref("extensions.tabsidebar.showoptions", false);
+pref("extensions.tabsidebar.showhelp", false);
